91be412e9d3b1299787fa1596a870cbc6d03c856
Enrique Contreras <rickiocontreras@gmail.com>
2025-07-16 00:23:48 -0300

    Crea ConvierteDatos

---
+++ src/main/java/com/aluracursos/screenmatch/sevice/ConvierteDatos.java
@@ -0,0 +1,8 @@
+package com.aluracursos.screenmatch.sevice;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ConvierteDatos {
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+}
