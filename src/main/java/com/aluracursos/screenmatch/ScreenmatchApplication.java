897d38c13fa20ee6dfa7ba3294cd8f0dc18af096
Enrique Contreras <rickiocontreras@gmail.com>
2025-07-16 00:47:24 -0300

    Convierte datos extraidos del Json

--- src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -1,6 +1,8 @@
 package com.aluracursos.screenmatch;
 
+import com.aluracursos.screenmatch.model.DatosSerie;
 import com.aluracursos.screenmatch.sevice.ConsumoApi;
+import com.aluracursos.screenmatch.sevice.ConvierteDatos;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,5 +19,8 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		var consumoApi = new ConsumoApi();
 		var json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=Grey%27s+Anatomy&apikey=855bfea4");
 		System.out.println(json);
+		ConvierteDatos conversor = new ConvierteDatos();
+		var datos = conversor.obtenerDatos(json, DatosSerie.class);
+		System.out.println(datos);
 	}
 }
