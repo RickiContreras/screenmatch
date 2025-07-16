b2908079b80b44648c91c704612c46e078b34c55
Enrique Contreras <rickiocontreras@gmail.com>
2025-07-16 11:37:55 -0300

    Actualiza ScreenmatchApplication

--- src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -1,5 +1,6 @@
 package com.aluracursos.screenmatch;
 
+import com.aluracursos.screenmatch.Principal.Principal;
 import com.aluracursos.screenmatch.model.DatosEpisodio;
 import com.aluracursos.screenmatch.model.DatosSerie;
 import com.aluracursos.screenmatch.model.DatosTemporadas;
@@ -21,24 +22,9 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=Grey%27s+Anatomy&apikey=855bfea4");
-		System.out.println(json);
-		ConvierteDatos conversor = new ConvierteDatos();
-		var datos = conversor.obtenerDatos(json, DatosSerie.class);
-		System.out.println(datos);
+		Principal principal = new Principal();
+		principal.muetraElMenu();
 
-		json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=Grey%27s+Anatomy&Season=1&episode=1&apikey=855bfea4");
-		DatosEpisodio episodios = conversor.obtenerDatos(json, DatosEpisodio.class);
-		System.out.println(episodios);
-
-		List<DatosTemporadas> temporadas = new ArrayList<>();
-		for (int i = 1; i <= datos.totalDeTemporadas() ; i++) {
-			json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=Grey%27s+Anatomy&Season="+i+"&apikey=855bfea4");
-			var datosTemporadas = conversor.obtenerDatos(json, DatosTemporadas.class);
-			temporadas.add(datosTemporadas);
-		}
-		temporadas.forEach(System.out::println);
 
 	}
 }
