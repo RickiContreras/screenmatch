6b5a181e058f3b5a576d08aa404390c981e43285
Enrique Contreras <rickiocontreras@gmail.com>
2025-07-16 21:29:11 -0300

    Actualiza ScreenmatchApplication

--- src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -23,7 +23,7 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		Principal principal = new Principal();
-		principal.muetraElMenu();
+		principal.muestraElMenu();
 
 
 	}
