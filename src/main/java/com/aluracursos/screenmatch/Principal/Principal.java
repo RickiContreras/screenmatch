2aec0243a3dd9dc4de94306f32236db5096fdb96
Enrique Contreras <rickiocontreras@gmail.com>
2025-07-18 08:07:51 -0300

    Actualiza clase Principal

--- src/main/java/com/aluracursos/screenmatch/Principal/Principal.java
+++ src/main/java/com/aluracursos/screenmatch/Principal/Principal.java
@@ -7,6 +7,8 @@ import com.aluracursos.screenmatch.model.Episodio;
 import com.aluracursos.screenmatch.sevice.ConsumoApi;
 import com.aluracursos.screenmatch.sevice.ConvierteDatos;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -68,6 +70,23 @@ public class Principal {
                         .map(d -> new Episodio(t.numero(), d)))
                 .collect(Collectors.toList());
         episodios.forEach(System.out::println);
+
+        //Busquda de episodios a partir de X año
+        System.out.println("Por favor indica el año de los episodios que quieres ver.");
+        var fecha = teclado.nextInt();
+        teclado.nextLine();
+
+        LocalDate fechaBusqueda = LocalDate.of(fecha, 1, 1);
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        episodios.stream()
+                .filter(e -> e.getFechaDeLanzamiento() != null && e.getFechaDeLanzamiento().isAfter(fechaBusqueda))
+                .forEach(e -> System.out.println(
+                        "Temporada " + e.getTemporada()
+                        + "Episodio " + e.getNumeroEpisodio()
+                        + "Fecha de lanzamiento " + e.getFechaDeLanzamiento().format(dtf)
+                ));
+
 
     }
 }
